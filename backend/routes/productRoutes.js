7f021c236c5e9902416a205200f6cd86cb060351
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-09 22:36:03 +0530

    first commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
